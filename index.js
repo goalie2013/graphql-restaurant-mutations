163ca273e8fcb6c911df16dff30ba71c9229f0bb
Gabriel Grinstein <gabriel.grinstein@gmail.com>
2022-08-31 00:17:50 -0700

    Updated correct files

--- index.js
+++ index.js
@@ -1,91 +1,143 @@
-var express = require("express");
-var { graphqlHTTP } = require("express-graphql");
-var { buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+const { buildSchema, assertInputType } = require("graphql");
+const express = require("express");
 
-var gameCatalogue = [
+// Construct a schema, using GraphQL schema language
+let restaurants = [
   {
     id: 1,
-    title: "Game B",
-    publisher: "Publisher ABC",
-    developer: "Developer DEF",
-    releaseDate: "2015-01-01",
-    platforms: [
-      { id: 1, name: "Xbox" },
-      { id: 2, name: "Playstation" },
-      { id: 3, name: "PC" },
+    name: "WoodsHill ",
+    description:
+      "American cuisine, farm to table, with fresh produce every day",
+    dishes: [
+      {
+        name: "Swordfish grill",
+        price: 27,
+      },
+      {
+        name: "Roasted Broccily ",
+        price: 11,
+      },
     ],
-    esrbRating: {
-      id: 1,
-      code: "E",
-      name: "Everyone",
-    },
   },
   {
     id: 2,
-    title: "Game C",
-    publisher: "Publisher ABC",
-    developer: "Developer DEF",
-    releaseDate: "2018-01-01",
-    platforms: [
-      { id: 1, name: "Xbox" },
-      { id: 3, name: "PC" },
+    name: "Fiorellas",
+    description:
+      "Italian-American home cooked food with fresh pasta and sauces",
+    dishes: [
+      {
+        name: "Flatbread",
+        price: 14,
+      },
+      {
+        name: "Carbonara",
+        price: 18,
+      },
+      {
+        name: "Spaghetti",
+        price: 19,
+      },
     ],
-    esrbRating: {
-      id: 1,
-      code: "E",
-      name: "Everyone",
-    },
   },
   {
     id: 3,
-    title: "Game A",
-    publisher: "Publisher ABC",
-    developer: "Developer DEF",
-    releaseDate: "2020-01-01",
-    platforms: [
-      { id: 1, name: "Xbox" },
-      { id: 2, name: "Playstation" },
+    name: "Karma",
+    description:
+      "Malaysian-Chinese-Japanese fusion, with great bar and bartenders",
+    dishes: [
+      {
+        name: "Dragon Roll",
+        price: 12,
+      },
+      {
+        name: "Pancake roll ",
+        price: 11,
+      },
+      {
+        name: "Cod cakes",
+        price: 13,
+      },
     ],
-    esrbRating: {
-      id: 2,
-      code: "M",
-      name: "Mature",
-    },
   },
 ];
-
-// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  type Query {
-    games: [Game]
-  },
-  type Game {
-    id: Int
-    title: String
-    publisher: String
-    developer: String
-    releaseDate: String
-    platforms: [Platform]
-    esrbRating: EsrbRating
+type Query{
+  restaurant(id: Int): restaurant
+  restaurants: [restaurant]
 },
-type EsrbRating {
-    id: Int
-    code: String
-    name: String
-},
-type Platform {
-    id: Int
-    name: String
+type restaurant {
+  id: Int
+  name: String
+  description: String
+  dishes:[Dish]
 }
- 
+type Dish{
+  name: String
+  price: Int
+}
+input restaurantInput{
+  name: String
+  description: String
+}
+type DeleteResponse{
+  ok: Boolean!
+}
+type Mutation{
+  setrestaurant(input: restaurantInput): restaurant
+  deleterestaurant(id: Int!): DeleteResponse
+  editrestaurant(id: Int!, name: String!): restaurant
+}
 `);
-
 // The root provides a resolver function for each API endpoint
+
 var root = {
-  games: () => gameCatalogue,
+  restaurant: (arg) => {
+    // Your code goes here
+    return restaurants[arg.id - 1];
+  },
+
+  restaurants: () => {
+    // Your code goes here
+    return restaurants;
+  },
+
+  setrestaurant: ({ input }) => {
+    // Your code goes here
+    const newRestaurant = {
+      name: input.name,
+      description: input.description,
+    };
+    restaurants = [...restaurants, newRestaurant];
+    return newRestaurant;
+  },
+
+  deleterestaurant: ({ id }) => {
+    let ok;
+    if (restaurants[id - 1]) {
+      ok = true;
+
+      restaurants = restaurants.filter((r) => r.id !== id);
+
+      return { ok };
+    }
+    ok = false;
+    return { ok };
+  },
+
+  editrestaurant: ({ id, ...restaurant }) => {
+    if (restaurants[id - 1]) {
+      restaurants[id - 1] = {
+        ...restaurants[id - 1],
+        ...restaurant,
+      };
+      return restaurants[id - 1];
+    }
+    return false;
+  },
 };
 
-var app = express();
+const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
@@ -94,5 +146,8 @@ app.use(
     graphiql: true,
   })
 );
-app.listen(4000);
-console.log("Running a GraphQL API server at http://localhost:4000/graphql");
+
+const port = 5500;
+app.listen(port, () => console.log("Running Graphql on Port:" + port));
+
+// export default root;
